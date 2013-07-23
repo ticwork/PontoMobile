4d4e3375ae0e9ad5493b3348f257929f3efb6aba
ticwork <ti@cwork.com.br>
2013-07-23 09:47:30 -0300

    TEMAS E TEMPLATES

---
+++ CordovaWP7_2_9_02/www/js/bd.js
@@ -0,0 +1,44 @@
+﻿// Populate the database 
+//
+function populateDB(tx) {
+    tx.executeSql('DROP TABLE IF EXISTS DEMO');
+    tx.executeSql('CREATE TABLE IF NOT EXISTS DEMO (id unique, data)');
+    tx.executeSql('INSERT INTO DEMO (id, data) VALUES (1, "First row")');
+    tx.executeSql('INSERT INTO DEMO (id, data) VALUES (2, "Second row")');
+}
+
+// Query the database
+//
+function queryDB(tx) {
+    tx.executeSql('SELECT * FROM DEMO', [], querySuccess, errorCB);
+}
+
+// Query the success callback
+//
+function querySuccess(tx, results) {
+    var len = results.rows.length;
+    console.log("DEMO table: " + len + " rows found.");
+    for (var i = 0; i < len; i++) {
+        console.log("Row = " + i + " ID = " + results.rows.item(i).id + " Data =  " + results.rows.item(i).data);
+    }
+}
+
+// Transaction error callback
+//
+function errorCB(err) {
+    console.log("Error processing SQL: " + err.code);
+}
+
+// Transaction success callback
+//
+function successCB() {
+    var db = window.openDatabase("Database", "1.0", "PhoneGap Demo", 200000);
+    db.transaction(queryDB, errorCB);
+}
+
+// PhoneGap is ready
+//
+function onDeviceReady() {
+    var db = window.openDatabase("Database", "1.0", "PhoneGap Demo", 200000);
+    db.transaction(populateDB, errorCB, successCB);
+}
